3e9094cf02bd48904a2d986f5492bd25005deb5b
Carlos Giron-Bran <carlos.gbran@gmail.com>
2018-10-04 12:17:33 -0400

    draw infinte square

--- template-p5-project/js/script.js
+++ template-p5-project/js/script.js
@@ -1,39 +1,24 @@
-
-var pointsMax=100;
-var points;
-var tx=0;
-var ty=0;
 var x=0;
-
-
+var y=0;
+var xSq = 0;
+var ySq = 0;
 function setup() {
-  createCanvas(1000,500);
+  createCanvas(500,500);
   background(51);
-  noStroke();
-  fill(random(255),random(255),random(255),175);
-
 }
 
 function draw() {
-if (x<width) {
-  drawSq();
-  console.log('drawing');
+var sqW=20;
+var sqH=sqW;
+var sqDistX = xSq+sqW;
+var sqDistY = ySq+sqW;
+
+while(xSq<width){
+rect(xSq,ySq,sqDistX,sqDistY);
+xSq+=sqW;
   }
-else {
-  resetAtt();
-  console.log('and another one!');
-}
 
+  ySq+=sqH;
+rect(xSq,ySq,sqDistX,sqDistY);
 
-}
-function drawSq(){
-  var y = height*noise(ty);
-  ellipse(x,y,5,5);
-  x+=0.75;
-  ty+= 0.01;
-}
-function resetAtt(){
-  x=0;
-  noStroke();
-  fill(random(255),random(255),random(255),175);
 }
