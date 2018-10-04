3af349a3506b75d18435cda8e877a4f72ca9ec78
Carlos Giron-Bran <carlos.gbran@gmail.com>
2018-10-04 12:41:58 -0400

    generate sq window

--- template-p5-project/js/script.js
+++ template-p5-project/js/script.js
@@ -2,23 +2,28 @@ var x=0;
 var y=0;
 var xSq = 0;
 var ySq = 0;
+
 function setup() {
   createCanvas(500,500);
   background(51);
 }
 
 function draw() {
-var sqW=20;
+var sqW=5;
 var sqH=sqW;
-var sqDistX = xSq+sqW;
-var sqDistY = ySq+sqW;
 
 while(xSq<width){
-rect(xSq,ySq,sqDistX,sqDistY);
+rect(xSq,ySq,sqW,sqH);
 xSq+=sqW;
   }
 
+  xSq=0;
   ySq+=sqH;
-rect(xSq,ySq,sqDistX,sqDistY);
+
+while(ySq<height && xSq<width){
+  console.log('nextline');
+  rect(xSq,ySq,sqW,sqH);
+  xSq+=sqW;
+}
 
 }
