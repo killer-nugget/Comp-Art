b8d0123921d886499291e3eeef9ef879d4987be5
Carlos Giron-Bran <carlos.gbran@gmail.com>
2018-10-04 12:44:53 -0400

    gen sq changed folder

---
+++ gen-sq/js/script.js
@@ -0,0 +1,29 @@
+var x=0;
+var y=0;
+var xSq = 0;
+var ySq = 0;
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background(51);
+}
+
+function draw() {
+var sqW=5;
+var sqH=sqW;
+
+while(xSq<width){
+rect(xSq,ySq,sqW,sqH);
+xSq+=sqW;
+  }
+
+  xSq=0;
+  ySq+=sqH;
+
+while(ySq<height && xSq<width){
+  console.log('nextline');
+  rect(xSq,ySq,sqW,sqH);
+  xSq+=sqW;
+}
+
+}
