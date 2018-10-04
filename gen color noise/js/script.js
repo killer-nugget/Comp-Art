45f7f5c496f98cd0b8f6ffb19baaf5604e5574f6
Carlos Giron-Bran <carlos.gbran@gmail.com>
2018-10-04 11:32:16 -0400

    add gen color1

---
+++ gen color noise/js/script.js
@@ -0,0 +1,40 @@
+//generate noise patterns random colors every time until death.
+
+var pointsMax=100;
+var points;
+var tx=0;
+var ty=0;
+var x=0;
+
+
+function setup() {
+  createCanvas(1000,500);
+  background(51);
+  noStroke();
+  fill(random(255),random(255),random(255),175);
+
+}
+
+function draw() {
+if (x<width) {
+  drawSq();
+  console.log('drawing');
+  }
+else {
+  resetAtt();
+  console.log('and another one!');
+}
+
+
+}
+function drawSq(){
+  var y = height*noise(ty);
+  ellipse(x,y,5,5);
+  x+=0.75;
+  ty+= 0.01;
+}
+function resetAtt(){
+  x=0;
+  noStroke();
+  fill(random(255),random(255),random(255),175);
+}
